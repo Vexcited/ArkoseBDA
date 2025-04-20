0b8818a9a2d9197797ecbf7af924e4632d0e109f
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-04-20 14:18:36 +0200

    chore: add unocss catppuccin preset

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig, presetWind4, transformerVariantGroup } from "unocss";
+import { presetCatppuccin } from 'unocss-catppuccin';
 
 export default defineConfig({
   presets: [
-    presetWind4()
+    presetWind4(),
+    presetCatppuccin({
+      prefix: false,
+      defaultFlavour: "macchiato"
+    })
   ],
   transformers: [
     transformerVariantGroup()
