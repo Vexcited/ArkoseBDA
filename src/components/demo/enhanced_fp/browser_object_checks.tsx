2fd918d92b05ddc98392bd1bec9f5db5924f8c43
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-11-12 09:35:13 +0100

    feat: add browser object checks

--- src/components/demo/enhanced_fp/browser_object_checks.tsx
+++ src/components/demo/enhanced_fp/browser_object_checks.tsx
@@ -1,3 +1,4 @@
+import { md5hex } from "../../../impl/utils";
 import Code from "../../Code";
 import Output from "../../Output";
 
@@ -6,13 +7,73 @@ const BrowserObjectChecksDemo = () => {
     <div>
       <Code
         snippet={`
-TODO
+() => {
+  const properties = [
+    "chrome",
+    "safari",
+    "__crWeb",
+    "__gCrWeb",
+    "yandex",
+    "__yb",
+    "__ybro",
+    "__firefox__",
+    "firefox",
+    "__edgeTrackingPreventionStatistics",
+    "webkit",
+    "oprt",
+    "samsungAr",
+    "ucweb",
+    "UCShellJava",
+    "puffinDevice",
+    "opr",
+  ].reduce(
+    (acc, curr) =>
+      window[curr] && typeof window[curr] === "object"
+        ? [].concat(acc, [curr])
+        : acc,
+    []
+  );
+
+  return properties.length > 0
+    ? md5hex(properties.sort().join(","))
+    : null;
+}
         `.trim()}
       />
 
       <Output
         generator={() => {
-          return void 0; // TODO
+          const properties = [
+            "chrome",
+            "safari",
+            "__crWeb",
+            "__gCrWeb",
+            "yandex",
+            "__yb",
+            "__ybro",
+            "__firefox__",
+            "firefox",
+            "__edgeTrackingPreventionStatistics",
+            "webkit",
+            "oprt",
+            "samsungAr",
+            "ucweb",
+            "UCShellJava",
+            "puffinDevice",
+            "opr",
+          ].reduce(
+            (acc, curr) =>
+              // @ts-expect-error
+              window[curr] && typeof window[curr] === "object"
+                ? // @ts-expect-error
+                  [].concat(acc, [curr])
+                : acc,
+            []
+          );
+
+          return properties.length > 0
+            ? md5hex(properties.sort().join(","))
+            : null;
         }}
       />
     </div>
